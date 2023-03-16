101cf5ad8c157255aeade50d497c29b2d7fa4a85
samirasingh19 <samira.singh021@gmail.com>
2023-03-16 16:55:12 +0530

    RandomPlayer

---
+++ RandomPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class RandomPlayer
+    {
+        public static void PlayerPosition()
+        {
+            Console.WriteLine("Stert the Snake and Ladder Game");
+            Console.WriteLine("Single player at start Position 0");
+            Random random = new Random();
+            int RollDice = random.Next(1, 6);
+            Console.WriteLine("Player rolls the dice and get: " + RollDice);
+        }
+    }
+}
