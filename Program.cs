e70be7ea40424f82e294ce13cd2b27e28cd1e1be
samirasingh19 <samira.singh021@gmail.com>
2023-03-16 16:50:57 +0530

    Snake&Ladder

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace SnakeAndLadder
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //SinglePlayerPosition.PlayerStart();
+            //RandomPlayer.PlayerPosition();
+           // RandomPositionSwitchCase.CasePosition();
+           // PlayerWinningPosition.ReachedHundred();
+            //PrintTotalDice.NumberOfDice();
+            //PlayerExactPosition.checkPosition();
+            //GameForTwoPlayer.TwoPlayer();
+
+        }
+    }
+}
