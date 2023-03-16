d4de09323eb786f04a98b036c90514c36b73eb48
samirasingh19 <samira.singh021@gmail.com>
2023-03-16 16:54:10 +0530

    SinglePlayerPosition

---
+++ SinglePlayerPosition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SinglePlayerPosition
+    {
+        public static void PlayerStart()
+        {
+            Console.WriteLine("Stert the Snake and Ladder Game");
+            Console.WriteLine("Singlr player at start Position 0");
+        }
+    }
+}
